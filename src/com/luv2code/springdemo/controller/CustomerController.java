a0dea4d82729137f8382ea84d1e3ec8e2585c97a
marco <amantil@protonmail.com>
2020-04-10 23:54:06 +0200

    aggiunti alcuni commenti al controller

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -10,15 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
 
-@Controller
-@RequestMapping("/customer")
+@Controller  // indico a spring che questo è un controller
+@RequestMapping("/customer")  //prima parte del path url web-customer-tracker/customer/
 public class CustomerController {
 
 	// need to inject the customer dao
-	@Autowired //spring will scan for a component that implements CustomerDAO interface
+	@Autowired //spring will scan for a component that implements CustomerDAO interface. dependency injection con java reflection. se ho piu
+	//di una classe che implementa quella interfaccia devo utilizzare anche l'annotazione @Qualifier con la specifica della classe che voglio
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	@RequestMapping("/list") //url path completo sarà web-customer-tracker/customer/list
 	public String listCustomers(Model theModel) {
 		
 		// get customers from the dao
