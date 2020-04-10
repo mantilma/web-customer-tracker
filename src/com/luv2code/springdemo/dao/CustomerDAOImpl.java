f8fc0f4b355c67e3b29476e72a9e56fda3aa7472
marco <amantil@protonmail.com>
2020-04-10 23:38:09 +0200

    primo controller, dao e view to get all customer

---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,45 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository // annotation per il DAO implementazioni
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the hibernate session factory
+	@Autowired //dependency injection
+	private SessionFactory sessionFactory;
+			
+	@Override
+	@Transactional //questa annotazione permette al nostro codice senza scrivere nulla di iniziare e finire una transazione nel db.
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+				
+		// create a query
+		Query<Customer> theQuery = 
+				currentSession.createQuery("from Customer", Customer.class);
+		
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+				
+		// return the results		
+		return customers;
+	}
+
+}
+
+
+
+
+
+
